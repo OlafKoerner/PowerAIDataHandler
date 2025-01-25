36fbdd1771e6a1bef00b99bdb7d8e513f5825dae
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2025-01-25 20:23:42 +0100

    test

--- PowerAIDataHandler.py
+++ PowerAIDataHandler.py
@@ -30,7 +30,7 @@ class ClassPowerAIDataHandler() :
             32: {'name': 'oven', 'minpow': 900, 'maxpow': 5500},
             #64: {'name': 'microwave', 'minpow': 800, 'maxpow': 1500},
             #128: {'name': 'kitchen-light', 'minpow': 250, 'maxpow': 500},
-            256: {'name': 'living-room-light', 'minpow': -self.base_pow, 'maxpow': 300}, # OKO for base load
+            #256: {'name': 'living-room-light', 'minpow': -self.base_pow, 'maxpow': 300}, # OKO for base load
             #512: {'name': 'dining-room-light', 'minpow': 0, 'maxpow': 400}, #OKO data looks strange ...
             #1024: {'name': 'ground-floor-light', 'minpow': 400},
             #2048: {'name': 'upper-floor-light', 'minpow': 180},
